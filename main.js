fbbc09a10ea068eb173e95560eae5e32ab27b1f8
15egreene <enriquemgreene@gmail.com>
2022-06-01 10:07:58 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// Question 1
+
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max", "HAS", "PuRple", "dog"]
+let dog_id = []
+/* write function takes in string
+and list of dog names
+checks if current name is in the string
+
+output should be 'Matched dog name' or 'no matches'
+
+*/
+
+function findWords(dog_string, dog_names) {
+    let dog_id = []
+    for (i in dog_string) {
+     if (dog_names in dog_string) {
+        return dog_id.push(i)
+     }
+}
+}
+console.log(dog)
+
+
+
+// Question 2
+let arr = ['Max', 'Baseball', 'reboot', 'goku']
+
+function word_up(x) {
+    if ( x % 2 === 0) {
+        return 'even index'
+    }
+
+   
+    }
+     
+console.log(word_up(arr))
