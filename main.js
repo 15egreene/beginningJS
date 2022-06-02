25e4e4f54db8fc00683ac68dedf06fb8ca3bbe99
15egreene <enriquemgreene@gmail.com>
2022-06-02 01:58:09 -0400

    wed night hmwk

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // Question 1
 
-let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
-let dog_names = ["Max", "HAS", "PuRple", "dog"]
-let dog_id = []
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!";
+let dog_names = ["Max", "HAS", "PuRple", "dog"];
 /* write function takes in string
 and list of dog names
 checks if current name is in the string
@@ -11,27 +10,41 @@ output should be 'Matched dog name' or 'no matches'
 
 */
 
-function findWords(dog_string, dog_names) {
-    let dog_id = []
-    for (i in dog_string) {
-     if (dog_names in dog_string) {
-        return dog_id.push(i)
-     }
-}
-}
-console.log(dog)
+// function findWords(str,names){
+//     for (let i = 0; i <names.length; i++){
+//        if (str.includes(names[i])){
+//             return 'Matched dog name'
+//        }
+//     }
+//     return 'No matches'
+// }
 
+// console.log(findWords(dog_string, dog_names))
+
+
+// let findWordsTwo = (str, names) => {
+//     let match = false;
+//     for (let i = 0; i < names.length; i++){
+//         if (str.includes(names[i])){
+//             console.log(`Matched dog_name ${names[i]}`);
+//             match=true;
+//         }
+//     }
+//     if (!match){
+//         console.log(`no_matches`);
+//     }
+// }
+
+// findWordsTwo(dog_string, dog_names)
 
 
 // Question 2
-let arr = ['Max', 'Baseball', 'reboot', 'goku']
-
-function word_up(x) {
-    if ( x % 2 === 0) {
-        return 'even index'
+let people = ['enrique', 'endiga', 'jack', 'bresha'];
+let replaceEvens = (arr) => {
+    for (let i =0; i < arr.length; i+=2){
+        arr.splice(i, 1,'even index');
     }
+}
 
-   
-    }
-     
-console.log(word_up(arr))
+replaceEvens(people);
+console.log(people);
---
+++ main2.js
@@ -0,0 +1,77 @@
+//==========Exercise #1 ===========//
+/*
+Write a function that parses through the below object and displays all of their
+favorite food dishes as shown:
+*/
+
+// let person3 = {
+//     pizza:["Deep Dish","South Side Thin Crust"],
+//     tacos:"Anything not from Taco bell",
+//     burgers:"Portillos Burgers",
+//     ice_cream:["Chocolate","Vanilla","Oreo"],
+//     shakes:[{
+//         oberwise:"Chocolate",
+//         dunkin:"Vanilla",
+//         culvers:"All of them",
+//         mcDonalds:"Sham-rock-shake",
+//         cupids_candies:"Chocolate Malt"
+//     }]
+// };
+// let keys = Object.keys(person3)
+// let val = Object.values(person3['shakes'][0])
+// let val1 = Object.values(person3)
+
+
+
+
+// val1.forEach((val1) => {
+//     console.log(` ${val1}`)
+// });
+
+
+
+//=======Exercise #2=========//
+/*
+Write an object prototype for a Person that has a name and age, has a
+printInfo method, and also has a method that 
+increments the persons age by 1 each time the method is called.
+Create two people using the 'new' keyword, and print 
+both of their infos and increment one persons
+age by 3 years. Use an arrow function for both methods
+*/
+class Wife {
+    constructor(name, age){
+        this.name = name;
+        this.age=age++;
+    }
+    printInfo(){
+        console.log(`${this.name} is ${this.age} years old.`)
+    }    
+}
+
+let rique = new Wife('Enrique', 25);
+rique.printInfo();
+
+
+// =============Exercise #3 ============//
+/*
+
+    Create a Promised based function that will check a string to determine if it's length is greater than 10.
+    If the length is greater than ten console log "Big word". 
+    If the length of the string is less than 10 console log "Small Number"
+*/
+
+
+let numCheck = string => {
+    return new Promise((resolve,reject) => {
+        if (string.length > 10){
+            console.log('Big word');
+            resolve(string); 
+        } else {
+            console.log('small number')
+            reject(string);
+        }
+    })
+}
+
+numCheck('Superman').then((result) => {console.log('it works i think')}).catch((result) => {console.log(`this doesnt work`)});
